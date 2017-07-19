96d7ec0e1ef5e35ee5cc781a09652884eeae8758
TimsManter <timsmanter@gmail.com>
2017-07-19 14:13:16 +0200

    Try to use postMessage

--- src/tsx/components/Hello.tsx
+++ src/tsx/components/Hello.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
+import Axios from "axios";
+import FJsonp from "fetch-jsonp";
 
 export interface HelloProps { compiler: string; framework: string; }
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
 export default class Hello extends React.Component<HelloProps, {}> {
+  componentDidMount() {
+    window.addEventListener("message", this.receiveMessage, false);
+
+    let preview = document.getElementById("document-preview");
+    if (preview !== null) {
+      (preview as HTMLIFrameElement).src = "https://1drv.ms/w/s!Ar9LEnsk58zwoZc28AYjLhOrQyUhqA";
+    }
+  }
+
+  receiveMessage(window: Window, event: MessageEvent) {
+    console.log(event);
+  }
+
   render() {
-    return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    return <iframe id="document-preview"></iframe>;
   }
 }
