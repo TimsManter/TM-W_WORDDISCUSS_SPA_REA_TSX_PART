454ff8b37e1f1d4e50326bb7e43adcb0017bd755
Krzysztof Ferenc <krzysztof.ferenc@billennium.pl>
2017-07-20 13:01:39 +0200

    Add base64 to blob converter function

--- src/tsx/components/Hello.tsx
+++ src/tsx/components/Hello.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as Docx from "docx4js";
+const doc = require("./test.docx");
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -7,13 +8,36 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the 'undefined' type.
 export default class Hello extends React.Component<HelloProps, {}> {
   componentDidMount() {
-    Docx.load("test.docx").then(docx => {
+    const blob: Blob = new Blob([doc], { type: "application/zip" });
+    (blob as any).name = "text.docx";
+    Docx.load(blob).then(docx => {
       let preview = document.getElementById("document-preview");
       if (preview !== null) {
         preview = docx.render();
       }
     });
   }
+
+  b64toBlob(b64Data, contentType="", sliceSize=512) {
+  const byteCharacters: string = atob(b64Data);
+  const byteArrays: Uint8Array[] = [];
+  
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+    
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+    
+    const byteArray = new Uint8Array(byteNumbers);
+    
+    byteArrays.push(byteArray);
+  }
+  
+  const blob = new Blob(byteArrays, {type: contentType});
+  return blob;
+}
 
   render() {
     return <div id="document-preview"></div>;
