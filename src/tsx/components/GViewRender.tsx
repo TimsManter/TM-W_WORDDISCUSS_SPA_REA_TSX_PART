78c4d3acc90bd4873aa2fba183a5b6de03887ebf
Krzysztof Ferenc <krzysztof.ferenc@billennium.pl>
2017-07-20 15:45:39 +0200

    Remove frame from iframe

--- src/tsx/components/GViewRender.tsx
+++ src/tsx/components/GViewRender.tsx
@@ -4,6 +4,6 @@ export interface P { }
 
 export default class GViewRender extends React.Component<P> {
   render() {
-    return <iframe id="document-preview" src="http://docs.google.com/viewer?url=https://calibre-ebook.com/downloads/demos/demo.docx&embedded=true"></iframe>;
+    return <iframe id="document-preview" src="http://docs.google.com/viewer?url=https://calibre-ebook.com/downloads/demos/demo.docx&embedded=true" frameBorder="0"></iframe>;
   }
 }
