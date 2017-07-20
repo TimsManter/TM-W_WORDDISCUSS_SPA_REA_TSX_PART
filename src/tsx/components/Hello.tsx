f6d814880dfed5bf49af73717e058291ad71b8d5
Krzysztof Ferenc <krzysztof.ferenc@billennium.pl>
2017-07-20 15:19:15 +0200

    Google docs test

--- src/tsx/components/Hello.tsx
+++ src/tsx/components/Hello.tsx
@@ -1,19 +1,22 @@
 import * as React from "react";
 import * as Docx from "docx4js";
-const doc = require("./test.docx");
+const doc = require("base64-loader!./test.docx");
 
 export interface HelloProps { compiler: string; framework: string; }
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
 export default class Hello extends React.Component<HelloProps, {}> {
-  componentDidMount() {
-    const blob: Blob = new Blob([doc], { type: "application/zip" });
+  /*componentDidMount() {
+    //console.log(doc);
+    const blob: Blob = this.b64toBlob(doc, "application/zip");
+    //console.log(blob);
     (blob as any).name = "text.docx";
     Docx.load(blob).then(docx => {
       let preview = document.getElementById("document-preview");
       if (preview !== null) {
-        preview = docx.render();
+        console.log(docx.render());
+        preview.innerHTML = docx.render();
       }
     });
   }
@@ -24,7 +27,7 @@ export default class Hello extends React.Component<HelloProps, {}> {
   
   for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
     const slice = byteCharacters.slice(offset, offset + sliceSize);
-    
+
     const byteNumbers = new Array(slice.length);
     for (let i = 0; i < slice.length; i++) {
       byteNumbers[i] = slice.charCodeAt(i);
@@ -41,5 +44,8 @@ export default class Hello extends React.Component<HelloProps, {}> {
 
   render() {
     return <div id="document-preview"></div>;
+  }*/
+  render() {
+    return <iframe src="http://docs.google.com/viewer?url=https://calibre-ebook.com/downloads/demos/demo.docx&embedded=true"></iframe>;
   }
 }
