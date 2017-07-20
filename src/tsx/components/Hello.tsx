6c60b1ebc61c2e3f52bd78d88f118f912fa8b25b
Krzysztof Ferenc <Krzysztof.Ferenc@Billennium.pl>
2017-07-20 10:21:01 +0200

    Prepare to use docx4js

--- src/tsx/components/Hello.tsx
+++ src/tsx/components/Hello.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import Axios from "axios";
-import FJsonp from "fetch-jsonp";
+import docx4js from "docx4js";
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -8,21 +7,10 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the 'undefined' type.
 export default class Hello extends React.Component<HelloProps, {}> {
   componentDidMount() {
-    //window.addEventListener("message", this.receiveMessage, false);
 
-    // let preview = document.getElementById("document-preview");
-    // if (preview !== null) {
-    //   (preview as HTMLIFrameElement).src = "https://1drv.ms/w/s!Ar9LEnsk58zwoZc28AYjLhOrQyUhqA";
-    // }
-
-     
   }
 
-  // receiveMessage(window: Window, event: WindowEventMap["message"]) {
-  //   console.log(event);
-  // }
-
   render() {
-    return <iframe sandbox="allow-same-origin allow-scripts" id="document-preview"></iframe>;
+    return <div id="document-preview"></div>;
   }
 }
