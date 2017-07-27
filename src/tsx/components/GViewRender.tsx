cc8983285cd13590e165ad9895771db5ee689c20
TM <github@timsmanter.net>
2017-07-27 10:30:50 +0200

    Fix gview frame size

--- src/tsx/components/GViewRender.tsx
+++ src/tsx/components/GViewRender.tsx
@@ -8,7 +8,7 @@ export default class GViewRender extends React.Component<P> {
   render() {
     const { url } = this.props;
 
-    return <iframe id="document-preview"
+    return <iframe id="gview-preview"
       src={`http://docs.google.com/viewer?url=${url}&embedded=true`}
       frameBorder="0">
     </iframe>;
