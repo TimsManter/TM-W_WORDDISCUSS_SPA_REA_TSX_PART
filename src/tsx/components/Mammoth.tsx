9f9e72d40110c37da4b513a5f975e7941128eaa4
TM <github@timsmanter.net>
2017-07-28 11:34:11 +0200

    Add comment marking

--- src/tsx/components/Mammoth.tsx
+++ src/tsx/components/Mammoth.tsx
@@ -3,7 +3,9 @@ import * as React from "react";
 export interface P {
   docHtml: string;
 }
-interface S { }
+interface S {
+  docHtml: string;
+}
 
 export default class Mammoth extends React.Component<P, S> {
   constructor(props: P) {
@@ -13,8 +15,25 @@ export default class Mammoth extends React.Component<P, S> {
     };
   }
 
+  componentWillReceiveProps(props: P) {
+    let html = props.docHtml;
+    if (html === "") { return; }
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(html, "text/html");
+    let commentStarts = doc.querySelectorAll("span[data-comment-edge=\"start\"]");
+    for (let s in commentStarts) {
+      if (isNaN(Number(s))) { continue; }
+      let id = commentStarts[s].getAttribute("data-comment-id");
+      commentStarts[s].innerHTML = "<mark>";
+      console.log(commentStarts[s]);
+      let commentEnd = doc.querySelector(`span[data-comment-edge=\"end\"][data-comment-edge=\"${id}\"]`);
+      if (commentEnd) { commentEnd.innerHTML = "</mark>"; }
+    }
+    this.setState({ docHtml: doc.documentElement.outerHTML });
+  }
+
   render() {
-    const { docHtml } = this.props;
+    const { docHtml } = this.state;
 
     return <div
       dangerouslySetInnerHTML={{__html: docHtml}}
