32ed7f37e8facb671917b3fe88ac7fa784b2463f
TM <github@timsmanter.net>
2017-07-31 08:28:22 +0200

    Apply DRY to class

--- src/tsx/components/CommentList.tsx
+++ src/tsx/components/CommentList.tsx
@@ -14,21 +14,12 @@ export default class CommentList extends React.Component<P> {
   renderCards(): JSX.Element[] {
     const comments = this.props.commentsHtml;
     let cards: JSX.Element[] = [];
-    const commentParts =
-      comments.split(/(?:<dt |<\/dd>)/i).filter(s => s !== "");
+    const commentParts = this.cleanSplit(comments, /(?:<dt |<\/dd>)/i);
     for (let c in commentParts) {
-      const title = commentParts[c]
-        .split(/id="[^>]*>|<\/dt>.*/i)
-        .filter(s => s !== "")[0];
-      const content = commentParts[c]
-        .split(/.*<p .*?>|<\/p.*/i)
-        .filter(s => s !== "")[0];
-      const commentRef = commentParts[c]
-        .split(/.*href="|">↑.*/i)
-        .filter(s => s !== "")[0];
-      const anchorId = commentParts[c]
-        .split(/id="|">.*$/i)
-        .filter(s => s !== "")[0];
+      const title = this.cleanSplit(commentParts[c], /id="[^>]*>|<\/dt>.*/i)[0];
+      const content = this.cleanSplit(commentParts[c], /.*<p .*?>|<\/p.*/i)[0];
+      const commentRef = this.cleanSplit(commentParts[c], /.*href="|">↑.*/i)[0];
+      const anchorId = this.cleanSplit(commentParts[c], /id="|">.*$/i)[0];
       let id = anchorId.split("-")[1];
       let comment = document.querySelector(`mark[data-comment-id=\"${id}\"]`) as HTMLElement;
       let offsetTop = 0;
@@ -53,6 +44,14 @@ export default class CommentList extends React.Component<P> {
   onMouseLeave(id) {
     let mark = document.querySelector(`mark[data-comment-id=\"${id}\"]`);
     if (mark) { mark.classList.remove("active"); }
+  }
+
+  cleanSplit(str: string, regex: RegExp): string[] {
+    return this.filterEmpty(str.split(regex));
+  }
+
+  filterEmpty(array: string[]): string[] {
+    return array.filter(s => s !== "");
   }
 
   render() {
