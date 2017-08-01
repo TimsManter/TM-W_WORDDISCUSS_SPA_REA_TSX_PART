b9b04e756190a361d7f7c241c0c04cc87bbc8bec
TM <github@timsmanter.net>
2017-08-01 11:46:16 +0200

    Add comments style

--- src/tsx/components/CommentList.tsx
+++ src/tsx/components/CommentList.tsx
@@ -3,6 +3,7 @@ import { List } from "office-ui-fabric-react/lib/List";
 import {
   FocusZone, FocusZoneDirection
 } from "office-ui-fabric-react/lib/FocusZone";
+import { CommandBar } from "office-ui-fabric-react/lib/CommandBar";
 import CommentsParser from "./../helpers/CommentsParser";
 import IComment from "./../model/IComment";
 
@@ -17,9 +18,9 @@ export default class CommentList extends React.Component<P> {
 
     return <FocusZone direction={FocusZoneDirection.vertical}>
       <List items={comments} onRenderCell={(comment: IComment, i) => (
-        <div key={i} className="comment-box" data-is-focusable={true}>
-          <h5>{comment.author}</h5>
-          <p>{comment.content}</p>
+        <div key={i} className="comment-box neutralLight" data-is-focusable={true}>
+          <h5 className="comment-author">{comment.author}</h5>
+          <p className="comment-content">{comment.content}</p>
           <ul>
             {!comment.responses ? [] : comment.responses.map((r, j) => (
               <li key={j}>{r.content}</li>
